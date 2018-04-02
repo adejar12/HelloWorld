3270a5622ec0d2d4cb6e13c446f58ed479d13558
Adejar Junior <adejarjunior18@gmail.com>
2018-04-02 17:04:21 -0300

    arrumando o mais 1

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
     public static void main(String[] args){
         for(int i=1;i<=20;i++){
-            System.out.println("Ola mundo ... " +1 );
+            System.out.println("Ola mundo ... " + i );
         }
     }
 }
