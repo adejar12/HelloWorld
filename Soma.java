d1d50c4827b01435f7becec11ee85edc1ae2082c
Adejar Junior <adejarjunior18@gmail.com>
2018-04-06 19:11:22 -0300

    adicionando a funcao retornoEmDoseTripla()

--- Soma.java
+++ Soma.java
@@ -7,4 +7,13 @@ public class Soma{
     public int retorno(){
         return a;
     }
+    // ESSE METODO ESTA NA LINHA MASTER
+    public int retornoEmDoseDupla(){
+        return a*2;
+    }
+
+    // ESSE METODO ESTA NA OUTRA LINHA
+    public int retornoEmDoseTripla(){
+        return a*3;
+    }
 }
